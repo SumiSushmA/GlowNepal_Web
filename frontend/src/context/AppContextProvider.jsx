57121800f40990e732ab21d49267ef4c7f215899
Sushma Sharma <154212342+SumiSushmA@users.noreply.github.com>
2025-01-30 22:26:22 +0545

    created appcontextprovider

---
+++ frontend/src/context/AppContextProvider.jsx
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import { createContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+export const AppContext = createContext();
+
+const AppContextProvider = (props) => {
+
+    const currencySymbol = '$';
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+    const [stylists, setStylists] = useState([]);
+    const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '');
+    const [userData, setUserData] = useState(false);
+
+    // Getting Stylists using API
+    const getStylistsData = async () => {
+        try {
+            const { data } = await axios.get(backendUrl + '/api/stylist/list');
+            if (data.success) {
+                setStylists(data.stylists);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
+    };
+
+    // Getting User Profile using API
+    const loadUserProfileData = async () => {
+        try {
+            const { data } = await axios.get(backendUrl + '/api/user/get-profile', { headers: { token } });
+
+            if (data.success) {
+                setUserData(data.userData);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
+    };
+
+    useEffect(() => {
+        getStylistsData();
+    }, []);
+
+    useEffect(() => {
+        if (token) {
+            loadUserProfileData();
+        }
+    }, [token]);
+
+    const value = {
+        stylists, getStylistsData,
+        currencySymbol,
+        backendUrl,
+        token, setToken,
+        userData, setUserData, loadUserProfileData
+    };
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    );
+};
+
+export default AppContextProvider;
+
+

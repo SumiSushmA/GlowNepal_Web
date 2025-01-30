b92908174b4229a893f1c29b47a26193c7fd7d64
Sushma Sharma <154212342+SumiSushmA@users.noreply.github.com>
2025-01-30 22:24:48 +0545

    env

---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import About from './pages/About'
+import Appointment from './pages/Appointment'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import MyAppointments from './pages/MyAppointments'
+import MyProfile from './pages/MyProfile'
+import Stylists from './pages/Stylists'
+import Verify from './pages/Verify'
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <ToastContainer />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/stylists' element={<Stylists />} />
+        <Route path='/stylists/:category' element={<Stylists />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/appointment/:stylistId' element={<Appointment />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/verify' element={<Verify />} />
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
